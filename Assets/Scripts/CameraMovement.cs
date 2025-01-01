7999d04336ba331cde3ade3166d6b1e9435fd772
ALZEE23 <muhamadalfarizial726@gmail.com>
2025-01-01 17:47:39 +0700

    feat: add Ui Scroll & Pinch Zoom

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CameraMovement : MonoBehaviour
 {
@@ -8,20 +9,27 @@ public class CameraMovement : MonoBehaviour
     [SerializeField]
     private Camera cam;
     private Vector3 dragOrigin;
+    public Slider zoomSlider;
+    private float initialPinchDistance;
+    private float initialCameraSize;
+    public bool isZooming = false;
+    public bool isPanning = false;
 
     void Start()
     {
-        
+        zoomSlider.onValueChanged.AddListener(OnZoomSliderValueChanged);
     }
 
     // Update is called once per frame
     void Update()
     {
         PanCamera();
+        ZoomCamera();
     }
 
-    private void PanCamera(){
-        if (Input.touchCount == 1)
+    private void PanCamera()
+    {
+        if (Input.touchCount == 1 && isPanning)
         {
             Touch touch = Input.GetTouch(0);
 
@@ -34,6 +42,32 @@ public class CameraMovement : MonoBehaviour
             {
                 Vector3 difference = dragOrigin - cam.ScreenToWorldPoint(touch.position);
                 cam.transform.position += difference;
+            }
+        }
+    }
+
+    private void OnZoomSliderValueChanged(float value)
+    {
+        cam.orthographicSize = Mathf.Lerp(2f, 10f, value);
+    }
+
+    private void ZoomCamera()
+    {
+        if (Input.touchCount == 2 && isZooming)
+        {
+            Touch touch1 = Input.GetTouch(0);
+            Touch touch2 = Input.GetTouch(1);
+
+            if (touch1.phase == TouchPhase.Began || touch2.phase == TouchPhase.Began)
+            {
+                initialPinchDistance = Vector2.Distance(touch1.position, touch2.position);
+                initialCameraSize = cam.orthographicSize;
+            }
+            else if (touch1.phase == TouchPhase.Moved || touch2.phase == TouchPhase.Moved)
+            {
+                float currentPinchDistance = Vector2.Distance(touch1.position, touch2.position);
+                float pinchRatio = initialPinchDistance / currentPinchDistance;
+                cam.orthographicSize = Mathf.Clamp(initialCameraSize * pinchRatio, 2f, 10f); // Adjust the min and max values as needed
             }
         }
     }
